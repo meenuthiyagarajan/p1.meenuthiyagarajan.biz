c752bbe0597e698c6fe5d8b1d092c8b2b269a528
Live Server <meenuthiyagarajan@g.harvard.edu>
2013-09-26 15:08:20 -0400

    One click deploying

---
+++ pull.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# The following two lines would be replicated for any other repository you wanted to be included in this 1-click-deploy
+cd /home/meenuthiyagarajan/www/p1.meenuthiyagarajan.biz
+git pull origin master
